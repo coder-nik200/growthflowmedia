d7006aac982b0499252915dbed2ba5c20af812e1
Coder_nik01 <nitishkb1707@gmail.com>
2026-03-23 15:46:23 +0530

    Vercel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,17 +2,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "sweetalert2/src/sweetalert2.scss";
 
-
-
-
-
 import AppRouter from "./app";
 
-
-createRoot(document.getElementById("root")).render(
-
-
-     <AppRouter/>
-      
-
-);
+createRoot(document.getElementById("root")).render(<AppRouter />);
